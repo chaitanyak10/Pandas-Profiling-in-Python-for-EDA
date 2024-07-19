2dbe5da4ae2eb586f9a1a9bb9fd497bf7d41bd5c
Chaitanya Kulkarni <chaitanya.ukulkarni@gmail.com>
2024-07-19 20:22:02 +0530

    This is Day1 File

---
+++ Python Folder/Day1.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+print('Hello VS Code!!!!')
+first_name = input('Enter your first name: ')
+last_name = input('Enter your last name: ')
+full_name = print(first_name + ' ' + last_name)
+print(full_name)
